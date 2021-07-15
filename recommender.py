02eddc5e5b1c5b46f0068e6a0e2614277574361b
MoneyMJ <65992386+MoneyMJ@users.noreply.github.com>
2021-07-15 20:03:14 +0530

    Clusters partially done

--- recommender.py
+++ recommender.py
@@ -6,9 +6,10 @@ from sklearn.cluster import KMeans
 ratings = pd.read_csv("ml-latest-small/ratings.csv")
 movies = pd.read_csv("ml-latest-small/movies.csv")
 
+ratings_unabridged = ratings
 
 
-print(ratings.columns)
+# print(ratings.columns)
 
 # userRatings = ratings.pivot_table(index = ['userId'], columns = ['title'], values = 'rating')
 # print(userRatings.head())
@@ -17,6 +18,7 @@ print(ratings.columns)
 movies['genres'] = movies.genres.str.split('|')
 ratings = pd.merge(movies, ratings)
 #print(movies.head())
+# print(ratings)
 
 all_genres = []
 
@@ -26,7 +28,7 @@ for i in range(len(movies.index)):
             all_genres.append(genre)
 
 all_genres.remove('(no genres listed)')
-print(all_genres)
+# print(all_genres)
 
 # print(ratings.head())
 
@@ -108,7 +110,7 @@ biased_data = biased_data[['Comedy', 'Sci-Fi']]
 # biased_data.set_index('num',inplace=True)
 
 # print(biased_data.head())
-print(biased_data)
+# print(biased_data)
 
 plt.scatter(biased_data['Comedy'],biased_data['Sci-Fi'])
 plt.xlabel('Komedi')
@@ -117,7 +119,7 @@ plt.title('Scatter')
 #plt.show()
 
 X=biased_data[['Comedy','Sci-Fi']]
-print(type(X))
+# print(type(X))
 
 """ Optimal Clusters : Atleast 5"""
 # num_cluster=list(range(1,9))
@@ -129,8 +131,10 @@ print(type(X))
 # plt.plot(num_cluster,inertias,'-o')
 #plt.show()
 
-model=KMeans(n_clusters=2)
+model=KMeans(n_clusters=19)
 predictions=model.fit_predict(X)
+# print(len(predictions))
+
 
 # def draw_clusters(biased_dataset, predictions, cmap='viridis'):
 #     fig = plt.figure(figsize=(8,8))
@@ -141,6 +145,7 @@ predictions=model.fit_predict(X)
 #     ax.set_ylabel('Avg romance rating')
 
 clustered = pd.concat([biased_data.reset_index(), pd.DataFrame({'group':predictions})], axis=1)
+# print(clustered)
 plt.figure(figsize=(8,8))
 plt.scatter(clustered['Comedy'], clustered['Sci-Fi'], c=clustered['group'], s=20)
 
@@ -148,9 +153,47 @@ plt.scatter(clustered['Comedy'], clustered['Sci-Fi'], c=clustered['group'], s=20
 # print(X.columns)
 # draw_clusters(biased_data,predictions)
 # print(predictions)
-plt.show()
+# plt.show()
+
+user_ratings = pd.merge(ratings_unabridged,movies[['movieId','title']],on='movieId')
+user_pivot=pd.pivot_table(user_ratings, index='userId', columns='title', values='rating')
+# print(user_pivot.head())
 
 
 
 
 
+
+# def get_most_rated_movies(user_movie_ratings, max_number_of_movies):
+#     # 1- Count
+#     user_movie_ratings = user_movie_ratings.append(user_movie_ratings.count(), ignore_index=True)
+#     # 2- sort
+#     user_movie_ratings_sorted = user_movie_ratings.sort_values(len(user_movie_ratings)-1, axis=1, ascending=False)
+#     user_movie_ratings_sorted = user_movie_ratings_sorted.drop(user_movie_ratings_sorted.tail(1).index)
+#     # 3- slice
+#     most_rated_movies = user_movie_ratings_sorted.iloc[:, :max_number_of_movies]
+#     return most_rated_movies
+
+# # Define the sorting by rating function
+# def sort_by_rating_density(user_movie_ratings, n_movies, n_users):
+#     most_rated_movies = get_most_rated_movies(user_movie_ratings, n_movies)
+#     most_rated_movies = get_users_who_rate_the_most(most_rated_movies, n_users)
+#     return most_rated_movies
+    
+# Define Function to get the most rated movies
+# list=[]
+# for i in range(len(20)):
+clusters = clustered.groupby('group')
+print(clusters.groups)
+
+arr_clusters = [[]]
+for i in range(0, clusters.ngroups - 1):
+    arr_clusters[i].append(clusters.get_group(str(i)))
+
+print(arr_clusters)
+
+
+
+
+
+# clusters = [[0 for k in range(ratings['userId'].max())] for j in range(len(all_genres))]
